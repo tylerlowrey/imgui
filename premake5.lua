771fa6f21b628db29a5692d1c2a8b2299c98bb15
Tyler Lowrey <contact@tylerlowrey.com>
2019-11-15 20:57:47 -0500

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -3,7 +3,7 @@ project "ImGui"
     language "C++"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
